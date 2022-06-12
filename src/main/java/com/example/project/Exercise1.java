268fe76a679e3ce421c71f4c6a2182eee7e3d3fc
AndreRH09 <91095214+AndreRH09@users.noreply.github.com>
2022-06-12 17:35:02 -0500

    Update Exercise1.java
    Intento1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,7 +16,39 @@ public class Exercise1 {
     public boolean esBalanceado(String s) {
         MyStack<Character> stack = new LinkedListStack<>();
         // Colocar codigo aqui
-
-        return false;
+                char aux;
+        
+        //Se recorre la longitud de la expresion
+        for (int i = 0; i < s.length(); i++) {
+            //Auxiliar para comparar/guardar
+            aux = s.charAt(i);
+            
+            //Se almacena solo si es Corchete Anidado Abierto (CAA)
+            if('('==aux || '['==aux || '{' == aux)
+                stack.push(aux);
+            
+            //Si se lee un ) 
+            if(')' == aux){
+                //Se verifica que el stack no este vacio, o preceda un CAA diferemnte a (
+                if(stack.isEmpty() || stack.top() == '{' || stack.top() == '[')
+                    return false;           //Si es asi retorna false;
+                stack.pop();
+            }
+            //Si se lee un ]
+            if(']' == aux){
+                //Se verifica que el stack no este vacio, o preceda un CAA diferemnte a [
+                if(stack.isEmpty() || stack.top() == '{' || stack.top() == '(')
+                    return false;
+                stack.pop();        //Se hace un pop del stack
+            }
+            //Si se lee un }
+            if('}' == aux){
+                //Se verifica que el stack no este vacio, o preceda un CAA diferemnte a {
+                if(stack.isEmpty() || stack.top() == '(' || stack.top() == '[')
+                    return false;
+                stack.pop();        //Se hace un pop del stack
+            }
+        }
+        return stack.isEmpty();     //Si el stack esta vacio, todos los CAA estan debidamente balanceados 
     }
 }
